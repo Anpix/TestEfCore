38f8386184670356e57eb1d602c6d2af3050c2ae
Rafael Tavares <rafael.tavares@iatec.com>
2021-01-29 16:59:47 -0300

    Running migration commands: Add-Migration Initial -c PessoaDbContext -o Migrations/PessoaDb Add-Migration Initial -c CarroDbContext -o Migrations/CarroDb

---
+++ Teste.Api/Migrations/CarroDb/20210129195557_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Teste.Api.Migrations.CarroDb
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carros");
+        }
+    }
+}
