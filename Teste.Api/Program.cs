66f19c30f0f428ed86ae1b1e4d0ea488e9dd441d
Rafael Tavares <rafael.tavares@iatec.com>
2021-01-29 17:33:14 -0300

    Run all migrations at system start

--- Teste.Api/Program.cs
+++ Teste.Api/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Teste.Data.Contexts;
 
 namespace Teste.Api
 {
@@ -13,7 +16,15 @@ namespace Teste.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetService<PessoaDbContext>().Database.Migrate();
+                scope.ServiceProvider.GetService<CarroDbContext>().Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
